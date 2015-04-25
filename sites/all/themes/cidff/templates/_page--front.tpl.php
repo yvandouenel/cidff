239ba8282c45000ec98eb257c44bdf6ca87cfbcd
Yvan Douënel <yvan@yvan-VPCEB3C5E.(none)>
2015-04-25 09:17:01 +0200

    Tpl modified.

--- sites/all/themes/cidff/templates/_page--front.tpl.php
+++ sites/all/themes/cidff/templates/_page--front.tpl.php
@@ -74,63 +74,121 @@
  */
 ?>
 <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
-  <div class="container">
-    <div class="navbar-header">
-      <?php if ($logo): ?>
-      <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" >
-        <img src="<?php print $logo; ?>" alt="Centre d'Information sur le Droit des Femmes et des Familles - retour accueil" />
-      </a>
-      <?php endif; ?>
+  <div class="container row">
+    <div class="col-md-5">
+      <div class="navbar-header">
 
-      <?php if (!empty($site_name)): ?>
-      <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
+        <?php if ($logo): ?>
+        <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        </a>
+        <?php endif; ?>
 
-      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    </div>
+          <?php if (!empty($site_name)): ?>
+          <h1 class="name navbar-brand"><a href="<?php print $front_page; ?>"><?php print $site_name; ?></a></h1>
+          <?php endif; ?>
+          <?php if (!empty($site_slogan)): ?>
+          <p class="slogan"><?php print $site_slogan; ?></p>
+          <?php endif; ?>
 
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse">
-        <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-        </nav>
+
+        <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
       </div>
-    <?php endif; ?>
+    </div>
+    <div class="col-md-7">
+      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+        <div class="navbar-collapse collapse">
+          <nav role="navigation" id="main-top-menu">
+            <?php if (!empty($primary_nav)): ?>
+              <?php print render($primary_nav); ?>
+            <?php endif; ?>
+            <?php if (!empty($secondary_nav)): ?>
+              <?php print render($secondary_nav); ?>
+            <?php endif; ?>
+            <?php if (!empty($page['navigation'])): ?>
+              <?php print render($page['navigation']); ?>
+            <?php endif; ?>
+          </nav>
+        </div>
+      <?php endif; ?>
+    </div>
   </div>
 </header>
 
 <div class="main-container container">
 
   <header role="banner" id="page-header">
+  	<!-- Site slogan removed
     <?php if (!empty($site_slogan)): ?>
       <p class="lead"><?php print $site_slogan; ?></p>
     <?php endif; ?>
-
+		-->
+		
     <?php print render($page['header']); ?>
   </header> <!-- /#page-header -->
+  
+  <div class="row">	
+  
+    <?php if (!empty($page['preface_first'])): ?>
+      <div class="col-lg-4" id="preface-first">
+      	<div class="panel panel-default">
+      		<div class="panel-heading">Espace Adolescents</div>
+      		<div class="panel-body">
+        		<?php print render($page['preface_first']); ?>
+        	</div>
+        </div>
+      </div>  <!-- /#preface-first -->
+    <?php endif; ?>
+    
+    <?php if (!empty($page['preface_second'])): ?>
+      <div class="col-lg-4" id="preface-second">
+      	<div class="panel panel-default">
+      		<div class="panel-heading">Espace Parents</div>
+      		<div class="panel-body">
+        		<?php print render($page['preface_second']); ?>
+        	</div>
+      	</div>
+      </div>  <!-- /#preface-second -->
+    <?php endif; ?>
+    
+    <?php if (!empty($page['preface_third'])): ?>
+      <div class="col-lg-4" id="preface-third">
+      	<div class="panel panel-default">
+      		<div class="panel-heading">Espace Professionnels</div>
+      		<div class="panel-body">
+        		<?php print render($page['preface_third']); ?>
+        	</div>
+        </div>
+      </div>  <!-- /#preface-third -->
+    <?php endif; ?>
+    
+  </div>
 
   <div class="row">
 
     <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-sm-3" role="complementary">
+      <aside class="col-sm-4" role="complementary">
         <?php print render($page['sidebar_first']); ?>
       </aside>  <!-- /#sidebar-first -->
     <?php endif; ?>
 
+    <?php
+    if (!empty($variables['page']['sidebar_first']) && !empty($variables['page']['sidebar_second'])) {
+    	$content_column_class = ' class="col-sm-4"';
+    }
+    elseif (!empty($variables['page']['sidebar_first']) || !empty($variables['page']['sidebar_second'])) {
+    	$content_column_class = ' class="col-sm-8"';
+    }
+    else {
+    	$content_column_class = ' class="col-sm-12"';
+    }
+    ?>
     <section<?php print $content_column_class; ?>>
       <?php if (!empty($page['highlighted'])): ?>
         <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
@@ -156,7 +214,7 @@
     </section>
 
     <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-sm-3" role="complementary">
+      <aside class="col-sm-4" role="complementary">
         <?php print render($page['sidebar_second']); ?>
       </aside>  <!-- /#sidebar-second -->
     <?php endif; ?>
--- sites/all/themes/cidff/templates/_page.tpl.php
+++ sites/all/themes/cidff/templates/_page.tpl.php
@@ -74,51 +74,60 @@
  */
 ?>
 <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
-  <div class="container">
-    <div class="navbar-header">
-      <?php if ($logo): ?>
-      <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" >
-        <img src="<?php print $logo; ?>" alt="Centre d'Information sur le Droit des Femmes et des Familles - retour accueil" />
-      </a>
-      <?php endif; ?>
+  <div class="container row">
+    <div class="col-md-5">
+      
+      <div class="navbar-header">
+        <?php if ($logo): ?>
+        <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        </a>
+        <?php endif; ?>
 
-      <?php if (!empty($site_name)): ?>
-      <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
+        <?php if (!empty($site_name)): ?>
+          <h1 class="name navbar-brand"><a href="<?php print $front_page; ?>" ><?php print $site_name; ?></a></h1>
+        <?php endif; ?>
+        <?php if (!empty($site_slogan)): ?>
+        <p class="slogan"><?php print $site_slogan; ?></p>
+        <?php endif; ?>
 
-      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    </div>
-
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse">
-        <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-        </nav>
+        <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
       </div>
-    <?php endif; ?>
+    </div>
+    <div class="col-md-7">
+      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+        <div class="navbar-collapse collapse">
+          <nav role="navigation" id="main-top-menu">
+            <?php if (!empty($primary_nav)): ?>
+              <?php print render($primary_nav); ?>
+            <?php endif; ?>
+            <?php if (!empty($secondary_nav)): ?>
+              <?php print render($secondary_nav); ?>
+            <?php endif; ?>
+            <?php if (!empty($page['navigation'])): ?>
+              <?php print render($page['navigation']); ?>
+            <?php endif; ?>
+          </nav>
+        </div>
+      <?php endif; ?>
+    </div>
   </div>
 </header>
 
 <div class="main-container container">
 
   <header role="banner" id="page-header">
+  	<!-- Site slogan removed
     <?php if (!empty($site_slogan)): ?>
       <p class="lead"><?php print $site_slogan; ?></p>
     <?php endif; ?>
+    -->
 
     <?php print render($page['header']); ?>
   </header> <!-- /#page-header -->
@@ -126,11 +135,22 @@
   <div class="row">
 
     <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-sm-3" role="complementary">
+      <aside class="col-sm-4" role="complementary">
         <?php print render($page['sidebar_first']); ?>
       </aside>  <!-- /#sidebar-first -->
     <?php endif; ?>
 
+    <?php
+    if (!empty($variables['page']['sidebar_first']) && !empty($variables['page']['sidebar_second'])) {
+    	$content_column_class = ' class="col-sm-4"';
+    }
+    elseif (!empty($variables['page']['sidebar_first']) || !empty($variables['page']['sidebar_second'])) {
+    	$content_column_class = ' class="col-sm-8"';
+    }
+    else {
+    	$content_column_class = ' class="col-sm-12"';
+    }
+    ?>
     <section<?php print $content_column_class; ?>>
       <?php if (!empty($page['highlighted'])): ?>
         <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
@@ -156,7 +176,7 @@
     </section>
 
     <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-sm-3" role="complementary">
+      <aside class="col-sm-4" role="complementary">
         <?php print render($page['sidebar_second']); ?>
       </aside>  <!-- /#sidebar-second -->
     <?php endif; ?>
