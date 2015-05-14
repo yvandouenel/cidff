9c991d330a96ef467b8c763f052b98bd661709fa
Yvan <y.douenel@coopernet.fr>
2015-05-14 18:27:27 +0200

    Template modified.

--- sites/all/themes/cidff/template.php
+++ sites/all/themes/cidff/template.php
@@ -4,3 +4,14 @@
  * @file
  * template.php
  */
+
+/*
+ * Supprime les attributs width et heigt des images
+ */
+function cidff_preprocess_image(&$variables) {
+  foreach (array('width', 'height') as $key) {
+  
+   unset($variables[$key]);
+   unset($variables[$key]);
+ }
+}
